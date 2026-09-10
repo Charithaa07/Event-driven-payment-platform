ee979fab1c1402fe566fa0b215f02bbaeb4f4530
Charithaa07 <shavvacharithareddy@gmail.com>
2026-09-10 10:14:32 -0700

    feat(outbox): enable scheduled event relay

--- payment-service/src/main/java/com/veda/payments/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/veda/payments/PaymentServiceApplication.java
@@ -2,7 +2,9 @@ package com.veda.payments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class PaymentServiceApplication {
     public static void main(String[] args) {
