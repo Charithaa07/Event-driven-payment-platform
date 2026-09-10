8ff4aacea651d52a8c22acac809587bf25aa90e9
Charithaa07 <shavvacharithareddy@gmail.com>
2026-09-10 10:39:04 -0700

    test: wire Kafka Testcontainer with dynamic properties

--- transaction-service/src/test/java/com/charitha/transactions/integration/PaymentEventFlowIntegrationTest.java
+++ transaction-service/src/test/java/com/charitha/transactions/integration/PaymentEventFlowIntegrationTest.java
@@ -14,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.kafka.KafkaContainer;
@@ -48,8 +50,12 @@ class PaymentEventFlowIntegrationTest {
             .withPassword("transactions");
 
     @Container
-    @ServiceConnection
     static KafkaContainer kafka = new KafkaContainer("apache/kafka-native:4.0.0");
+
+    @DynamicPropertySource
+    static void kafkaProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+    }
 
     @Autowired
     private KafkaTemplate<Object, Object> kafkaTemplate;
